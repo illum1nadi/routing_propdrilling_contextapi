873c3406aa2a8409aca4573568241b39da3c4c48
illum1nadi <adityainduri@gmail.com>
2025-04-02 16:21:33 +0530

    routing using an array of routes.

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -1,17 +1,32 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Dashboard } from './components/Dashboard';
+import { Landing } from './components/Landing';
 
 function App() {
+  const router = [
+    {
+      route: "/",
+      component: Dashboard
+    },
+    {
+      route: "/dashboard",
+      component: Landing
+    }
+  ];
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
-        <Route path='/' element={<Landing/>}></Route>
+        {router.map((route, index) => (
+          <Route 
+            key={index} 
+            path={route.route} 
+            element={<route.component />} 
+          />
+        ))}
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

7094e3ddaab52a7415610501f7a4b159e4348ae5
illum1nadi <adityainduri@gmail.com>
2025-04-02 18:26:40 +0530

    Why we drill down props.

--- propdrilling/src/App.jsx
+++ propdrilling/src/App.jsx
@@ -5,8 +5,33 @@ function App() {
 
   return (
     <>
-     
+      <Count count={count} />
+      
     </>
+  )
+}
+
+//setCount was unecessary for count but buttons needed it.
+function Count({count, setCount}) {
+  return (
+    <div>
+      {count}
+      <Buttons count = {count} setCount={setCount} />
+    </div>
+  )
+}
+
+function Buttons({count, setCount}) {
+  return (
+    <div>
+      <button onClick={() => {
+
+      }}>Increase</button>
+
+      <button onClick={() => {
+        
+      }}>Decrease</button>
+    </div>
   )
 }
 
